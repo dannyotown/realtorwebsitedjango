fc4516119e5c99bda8eefa97314304149128c06b
Dtown <dannyotown@gmail.com>
2020-10-17 10:53:49 -0500

    Init commit

--- main/main/settings.py
+++ main/main/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^#(ewk5hzj-%x6da#oui@@jtms0ov0p2aiib-n)jng@$8@3v48'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
