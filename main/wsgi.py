2088fd3de5ae5eeeaf85ffd4aa78e6328ebd8b7f
Dtown <dannyotown@gmail.com>
2020-10-17 11:26:30 -0500

    WSGI file fix for AWS

--- main/wsgi.py
+++ main/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
 application = get_wsgi_application()
