cdfce2c4411e60d2cdc9f01cb7c61656cccc8536
Dtown <dannyotown@gmail.com>
2020-10-17 17:54:32 -0500

    removed settings for http

--- main/settings.py
+++ main/settings.py
@@ -115,8 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 SECURE_SSL_REDIRECT = False 
 SESSION_COOKIE_SECURE = False 
-SESSION_COOKIE_HTTPONLY= False 
-CSRF_COOKIE_SECURE = False 
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
