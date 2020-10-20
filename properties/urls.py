e9240a7c6696cf6910339fb8a669c2c5fdd19a0e
Dtown <dannyotown@gmail.com>
2020-10-20 10:33:53 -0500

    Removed Serializer and changed migration

--- properties/urls.py
+++ properties/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("property/", views.propertyList, name="property-list"),
-    path("property/<str:pk>", views.propertyOne, name="property-one"),
-    # path("property-create/", views.propertyCreate, name="property-create"),
-    # path("property-delete/<str:pk>", views.propertyDelete, name="property-delete"),
+    path("property/", views.getPropertyList, name="property-list"),
+    path("property/<str:pk>", views.getProperty, name="property-list")
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,67 +1,17 @@
-from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import PropertySerializer
 from .models import Property
-from django.contrib.auth.decorators import user_passes_test
 
 # GET ALL PROPERTIES.
-@api_view(["GET"])
-def propertyList(request):
+def getPropertyList(request):
     try:
-        prop = Property.objects.all()
-        serializer = PropertySerializer(prop, many=True)
-        return Response(serializer.data)
+        props = list(Property.objects.values().order_by('priority'))
+        return JsonResponse(props, safe=False)
     except:
-        return HttpResponse("Server Error", status=404)
+        return JsonResponse({'Error':'Server Error'}, status=400)
 
-
-# GET ONE PROPERTY.
-@api_view(["GET"])
-def propertyOne(request, pk):
+def getProperty(request, pk):
     try:
-        prop = Property.objects.get(property_id=pk)
-        serializer = PropertySerializer(prop, many=False)
-        return Response(serializer.data)
+        props = list(Property.objects.values().filter(property_id=pk))
+        return JsonResponse(props, safe=False)
     except:
-        return HttpResponse("Server Error", status=404)
-
-
-# # CREATE A PROPERTY.
-# @api_view(["POST"])
-# @user_passes_test(lambda u: u.is_superuser)
-# def propertyCreate(request):
-#     try:
-#         serializer = PropertySerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#         return Response(serializer.data)
-#     except:
-#         return HttpResponse("Server Error", status=404)
-
-
-# # UPDATE A PROPERTY.
-# @api_view(["POST"])
-# @user_passes_test(lambda u: u.is_superuser)
-# def propertyUpdate(request, pk):
-#     try:
-#         prop = Property.objects.get(property_id=pk)
-#         serializer = PropertySerializer(instance=task, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#         return Response(serializer.data)
-#     except:
-#         return HttpResponse("Server Error", status=404)
-
-
-# # DELETE A PROPERTY.
-# @api_view(["DELETE"])
-# @user_passes_test(lambda u: u.is_superuser)
-# def propertyDelete(request, pk):
-#     try:
-#         prop = Property.objects.get(property_id=pk)
-#         prop.delete()
-#         return HttpResponse("Property Deleted", status=202)
-#     except:
-#         return HttpResponse("Server Error", status=404)
+        return JsonResponse({'Error':'Server Error'}, status=400)
