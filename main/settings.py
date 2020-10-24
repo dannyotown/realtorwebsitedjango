e110a623ac31b2bf62b0808788218aa444e048c2
Dtown <dannyotown@gmail.com>
2020-10-24 12:41:45 -0500

    Added postgres back

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('SECRET_KEY') == "False"
+DEBUG = os.getenv('PROD') == "False"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -78,12 +78,24 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if os.getenv('PROD') == 'True':
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.getenv('RDS_DB_NAME'),
+            "USER": os.getenv('RDS_USERNAME'),
+            "PASSWORD": os.getenv('RDS_PASSWORD'),
+            "HOST": os.getenv('RDS_HOSTNAME'),
+            "PORT": os.getenv('RDS_PORT')
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
